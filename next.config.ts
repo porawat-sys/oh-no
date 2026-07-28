4516c4c62bb6853f3d77d4a5c71add5d1ecb06e8
porawat-sys <porawat.suk@pccpl.ac.th>
2026-07-28 08:40:15 +0000

    fix basePath in next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: 'OH-NO',   // ใส่ชื่อ repo ของคุณตรงนี้
+  basePath: '/oh-no',
   images: {
     unoptimized: true,
   },
